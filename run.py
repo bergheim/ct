31d579c454fb9cef34a2e922b838b319c5e41f67
Thomas Bergheim <thomas.bergheim@bouvet.no>
2011-07-27 10:29:12 +0200

    Integrated KVSession and Flask-Login

--- run.py
+++ run.py
@@ -2,15 +2,22 @@
 
 import calendar
 import datetime
+import operator
+import pprint
 from collections import defaultdict
+from functools import wraps
 
 from flask import Flask, request, Response, session, g, redirect, url_for, \
      abort, render_template, flash, get_flashed_messages, current_app
 from flaskext.wtf import Form, DateField, TextField, Required, validators, PasswordField, TextAreaField, DecimalField
 import ConfigParser, random
-from flaskext.login import LoginManager, UserMixin, \
-    login_required, login_user, logout_user
+#from flaskext.login import LoginManager, UserMixin, \
+#    login_required, login_user, logout_user
+from simplekv.memory import DictStore
+from flaskext.kvsession import KVSessionExtension, KVSession
 from ct.apis import RangeAPI
+
+pp = pprint.PrettyPrinter(indent=4)
 
 def debug():
     assert current_app.debug == False, "Debug:"
@@ -34,25 +41,73 @@ class MethodRewriteMiddleware(object):
         return self.app(environ, start_response)
 
 
-app = Flask(__name__)
 config = ConfigParser.ConfigParser()
 config.read(["config.ini.sample", "config.ini"])
+
+app = Flask(__name__)
 app.config['DEBUG'] = config.get("server", "debug")
 app.config['SECRET_KEY'] = config.get("server", "secret_key")
-app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
 
-login_manager = LoginManager()
-login_manager.setup_app(app)
-login_manager.login_view = "login"
+store = DictStore()
+kvsession = KVSessionExtension(store, app)
+#app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
 
-class UserLogin(UserMixin):
+#login_manager = LoginManager()
+#login_manager.setup_app(app)
+#login_manager.login_view = "login"
+
+def generate_calendar_month(date):
+    c = calendar.Calendar()
+    year = date.year
+    month = date.month
+    days = []
+    for day in c.itermonthdays2(year, month):
+        days.append(day)
+
+    weeknumbers = []
+    for day in c.monthdays2calendar(year, month):
+        weekday,_ = day[0]
+
+        if weekday == 0:
+            weekday = 1
+
+        weeknumbers.append(datetime.date(year, month, weekday).isocalendar()[1])
+
+
+    cal = {}
+    weeknumber = weeknumbers.pop(0)
+    cal[weeknumber] = []
+    for day, weekday in days:
+        cal[weeknumber].append([day, weekday])
+
+        if day > 0 and weekday == 6 and len(weeknumbers) > 0:
+            weeknumber = weeknumbers.pop(0)
+            cal[weeknumber] = []
+
+    return cal
+
+
+#class User(UserMixin):
+class User():
     def __init__(self, id):
         self.id = id
-        self.username = 'user' + str(id)
-        self.password = self.username
+        #if "user" in session:
+        #    self.name = session['user']
+        #else:
+        self.username = self.name = str(id)
+
+        self.password = self.name + "_secret"
 
     def __repr__(self):
-        return "%d: %s/%s" % (self.id, self.username, self.password)
+        return "%s: %s" % (self.id, self.username)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not valid_user() or not session['ct'].valid_session():
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 class UserForm(Form):
     username = TextField(u'Brukernavn', [validators.Length(min=3, max=25)], [], u'Ditt brukernavn')
@@ -94,58 +149,8 @@ class RangeForm(Form):
         format='%d.%m.%Y')
 
 
-class Hours(object):
-    def __init__(self, id = None, name = None):
-        self.id = id
-        self.name = name
-
-class Activity(object):
-    projects = []
-    projects.append( {'name': 'Lunch', 'activity': [] } )
-    projects.append( {'name': 'Internprosjekt', 'activity': [] } )
-
-    projects[0]['activity'] = [
-            { 'hours': 4, 'note': 'ingen' },
-            { 'hours': 2, 'note': '' },
-            { 'hours': 8, 'note': '' },
-            { 'hours': 7, 'note': 'gjorde ditt, and then datt' },
-            { 'hours': 2, 'note': 'dro tidlig' },
-            { 'hours': 3, 'note': 'helgejobbing' }
-            ]
-
-    projects[1]['activity'] = [
-            { 'hours': 1, 'note': '' },
-            { 'hours': 2, 'note': '' },
-            { 'hours': 3, 'note': '' },
-            { 'hours': 4, 'note': '' },
-            { 'hours': 5, 'note': 'lol' },
-            { 'hours': 6, 'note': '' }
-            ]
-
-    #def __init__(self):
-    #    for p in self.projects:
-    #        activity = []
-    #        for day in xrange(1,7):
-    #            a = { 'hours': random.randint(0,7), 'note': "" }
-    #            if a['hours'] is not 0 and random.random() > 0.7:
-    #                a['note'] = 'random note'
-
-    #            activity.append(a)
-
-    #        p['activity'] = activity
-
-testAct = Activity()
-
-def valid_login(username, password):
-    if not User.query.filter_by(username=username,password=password).first():
-        return False
-
-    return True
-
 def valid_user():
-    if 'username' in session:
-        return True
-    return False
+    return 'user' in session
 
 def redirect_if_invalid_user():
     if not valid_user():
@@ -156,38 +161,43 @@ def redirect_if_invalid_user():
 def redirect_to_login():
     return redirect(url_for('login'))
 
+def do_ct_login(username, password):
+    server = config.get("server", "ct_url")
+    ct = RangeAPI(server)
+    logged_in = ct.login(username, password)
+    if logged_in:
+        session['user'] = username
+        session['ct'] = ct
+        #FIXME: for some reason this does not work
+        #session['projects'] = ct.get_projects()
+
+    return logged_in
+
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
-
-def do_ct_login(form):
-    server = config.get("server", "ct_url")
-    ct = RangeAPI(server)
-    logged_in = ct.login(form.username.data, form.password.data)
-    if logged_in:
-        session['user'] = form.username.data
-        session['ct'] = ct
-
-    return logged_in
 
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = UserForm()
     error = ""
     if request.method == 'POST' and form.validate():
-        logged_in = do_ct_login(form)
-        g.user = UserLogin(form.username.data)
-        login_user(g.user)
+        username = form.username.data
+        password = form.password.data
+        logged_in = do_ct_login(username, password)
+        g.user = User(username)
+        #login_user(g.user)
         if logged_in:
-            return redirect(request.args.get("next") or url_for('show_user', username=g.user.username))
+            return redirect(request.args.get("next") or url_for('show_user', username=username))
 
     return render_template('login.html', form=form, error=error)
 
 @app.route('/logout')
-@login_required
 def logout():
+    print session
     logout_user()
+    session.clear()
     return redirect(url_for('index', _external=True), 301)
 
 @app.errorhandler(401)
@@ -211,35 +221,77 @@ def view_day(date):
 
 @app.route('/view/week', methods=['GET'])
 @login_required
-def view_week():
+def view_current_week():
     projects = testAct.projects
 
     return render_template('view_week.html', projects=projects, random=random.randint(1,1000))
 
-@app.route('/view/month', methods=['GET'])
+@app.route('/view/week/<int:week>', methods=['GET'])
 @login_required
-def view_month():
-    ct = session['ct']
-    form = RangeForm(formdata=request.args)
-
-    now = datetime.datetime.now()
-
-    c = calendar.LocaleHTMLCalendar(calendar.MONDAY)
-    calendar_month = c.formatmonth(now.year, now.month)
-    calendar_month = c.monthdays2calendar(now.year, now.month)
-
-    calendar_header = c.formatweekheader()
-
-    app.logger.debug(form.to_date.data)
-
+def view_week(week):
     activities = ct.get_activities(form.from_date.data, form.to_date.data)
-
-
     days = defaultdict(lambda: [])
     for activity in activities:
         days[activity.day].append(activity)
 
-    return render_template('view_month.html', calendar=calendar_month, calendar_header=calendar_header)
+    return render_template('view_week.html', projects=[], random=random.randint(1,1000))
+
+
+@app.route('/view/month', methods=['GET'])
+@login_required
+def view_current_month():
+    now = datetime.datetime.now()
+    date = "%s.%s"  % (now.year, now.month)
+    return view_month(date)
+
+@app.route('/view/month/<month>', methods=['GET'])
+@login_required
+def view_month(month):
+    ct = session['ct']
+    #form = RangeForm(formdata=request.args)
+
+    #now = datetime.datetime.now()
+    #c = calendar.LocaleHTMLCalendar(calendar.MONDAY)
+    #calendar_month = c.formatmonth(now.year, now.month)
+    #calendar_month = c.monthdays2calendar(now.year, now.month)
+    #calendar_header = c.formatweekheader()
+
+
+    year, month = month.split(".")
+    year = int(year)
+    month = int(month)
+    date = datetime.date(year, month, 1)
+    prev_month = date - datetime.timedelta(1)
+    prev_month = "%s.%02d" % (prev_month.year, int(prev_month.month))
+    next_month = date + datetime.timedelta(40)
+    next_month = "%s.%02d" % (next_month.year, int(next_month.month))
+
+    todate = date + datetime.timedelta(calendar.monthrange(year, month)[1]-1)
+
+    calendar_month = generate_calendar_month(date)
+
+
+    activities = ct.get_activities(date, todate)
+    days = defaultdict(lambda: [])
+    for activity in activities:
+        days[activity.day].append(activity)
+
+
+    pp.pprint(session)
+
+    work_month = {}
+    for week in calendar_month:
+        work_month[week] = []
+        for day_month, day_week in calendar_month[week]:
+            if day_month == 0:
+                work_month[week].append({ "day": day_month, "weekday": day_week, "activities": None })
+            else:
+                work_month[week].append({ "day": day_month, "weekday": day_week, "activities": days[datetime.date(year, month, day_month)] })
+
+    work_month = sorted(work_month.iteritems(), key=operator.itemgetter(0))
+
+
+    return render_template('view_month.html', calendar=work_month, next_month=next_month, prev_month=prev_month)
 
 @app.route('/activity/<id>', methods=['GET', 'POST'])
 @login_required
@@ -267,10 +319,10 @@ def hello_test():
 def show_user(username):
     return render_template('user.html', username=username)
 
-@login_manager.user_loader
-def load_user(userid):
-    g.user = UserLogin(userid)
-    return g.user
+#@login_manager.user_loader
+#def load_user(userid):
+#    g.user = User(userid)
+#    return g.user
 
 
 if __name__ == '__main__':

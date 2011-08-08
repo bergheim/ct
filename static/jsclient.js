4af5495b2590aa23569652d4716a2cd89528e524
Thomas Bergheim <thomas.bergheim@bouvet.no>
2011-08-08 15:33:01 +0200

    Further development of prototype
    Added missing library files, created mocked list of activities, removed
    demo-code, etc.

---
+++ static/jsclient.js
@@ -0,0 +1,9 @@
+var activities = [          
+    { id: 1, name: 'Lunsj', comment: '', duration: 0.5 },
+    { id: 2, name: 'Ferie', comment: 'Dro til Italia', duration: 8.0 },
+    { id: 3, name: 'Avspasering', comment: '', duration: 7.50 }
+];
+
+var viewModel = {
+}
+ko.applyBindings(viewModel);

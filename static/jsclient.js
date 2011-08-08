0cc4f372deed4d1c41f88e7e1e2c37036986e8b7
Thomas Bergheim <thomas.bergheim@bouvet.no>
2011-08-08 16:00:47 +0200

    Added available activities and non-working buttons

--- static/jsclient.js
+++ static/jsclient.js
@@ -1,3 +1,9 @@
+var availableActivities = [          
+    { id: 1, name: 'Lunsj', comment: '', duration: 0.5 },
+    { id: 2, name: 'Ferie', comment: 'Dro til Italia', duration: 8.0 },
+    { id: 3, name: 'Avspasering', comment: '', duration: 7.50 }
+];
+
 var activities = [          
     { id: 1, name: 'Lunsj', comment: '', duration: 0.5 },
     { id: 2, name: 'Ferie', comment: 'Dro til Italia', duration: 8.0 },

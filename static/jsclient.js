377100074d415cd175de81225be8b90e418ec312
Alf Lervag <alf.lervag@bouvet.no>
2011-08-09 11:40:48 +0200

    Created a viewModel and activated the buttons to enable navigation

--- static/jsclient.js
+++ static/jsclient.js
@@ -1,19 +1,77 @@
-var allActivities;
-var activities, availableActivities, projects;
+(function() {
+    var formatDate = function(d) {
+	return d.toString("yyyy-MM-dd");
+    };
 
-$(document).bind("ready", function() {
-        $.getJSON('/api/projects', function(data) {
-            projects = data.projects;
-        });
+    var currentDay = function() {
+	return formatDate(Date.today());
+    };
 
-        $.getJSON('/api/activities/2011/8', function(data) {
-            allActivities = data.activities;
-            activities = allActivities['2011-08-08']
-            availableActivities = allActivities['2011-08-06']
+    var nextDayFromString = function(s) {
+	var d = new Date(s).add(1).day();
+	return formatDate(d);
+    };
 
-            var viewModel = {
-            }
-            ko.applyBindings(viewModel);
-            $('.databound').each(function() { $(this).page() });
-        });
-});
+    var previousDayFromString = function(s) {
+	var d = new Date(s).add(-1).day();
+	return formatDate(d);
+    };
+
+    var populateProjects = function(viewModel) {
+	$.getJSON('/api/projects', function(data) {
+	    viewModel.projects(data.projects);
+	});
+    };
+
+    var populateActivities = function(viewModel) {
+	$.getJSON('/api/activities/2011/8', function(data) {
+	    viewModel.activitiesByDay(data.activities);
+	});
+    };
+	
+    $(document).bind("ready", function() {
+	var viewModel = {
+	    // Data
+	    projects: ko.observable([]),
+	    activitiesByDay: ko.observable({}),
+	    selectedDay: ko.observable(currentDay()),
+
+	    // Behaviours
+	    gotoToday: function() {
+		this.selectedDay(currentDay());
+	    },
+
+	    gotoNextDay: function () { 
+		var day = this.selectedDay();
+		this.selectedDay(nextDayFromString(day));
+	    },
+
+	    gotoPreviousDay: function () { 
+		var day = this.selectedDay();
+		this.selectedDay(previousDayFromString(day));
+	    },
+
+	    reloadData: function() {
+		populateProjects(this);
+		populateActivities(this);
+	    }
+	};
+
+	viewModel.currentActivities = ko.dependentObservable(function() {
+	    return this.activitiesByDay()[this.selectedDay()] || [];
+	}, viewModel);
+
+	viewModel.recentActivities = ko.dependentObservable(function() {
+	    var day = previousDayFromString(this.selectedDay());
+	    return this.activitiesByDay()[day] || [];
+	}, viewModel);
+
+	viewModel.selectedDayString = ko.dependentObservable(function() {
+	    var d = new Date(this.selectedDay());
+	    return d.toString("dddd d. MMMM yyyy").toLowerCase();
+	}, viewModel);
+
+	ko.applyBindings(viewModel);
+	// $('.databound').each(function() { $(this).page() });
+    });
+}());

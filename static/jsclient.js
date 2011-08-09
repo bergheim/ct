22273d79cc6588e0dcf2beeacf3cf9ed05a561bf
Alf Lervag <alf.lervag@bouvet.no>
2011-08-09 17:24:46 +0200

    Got a working styled version but no history

--- static/jsclient.js
+++ static/jsclient.js
@@ -17,19 +17,49 @@
 	return formatDate(d);
     };
 
+    var populateFromCache = function(viewModel) {
+	// Fetch cached values
+	var storage = localStorage || [];
+	var projects = localStorage['projects'];
+	if (projects) {
+	    var parsed = JSON.parse(projects);
+	    viewModel.projects(parsed);
+	}
+
+	var activities = localStorage['activities'];
+	if (activities) {
+	    var parsed = JSON.parse(activities);
+	    viewModel.activitiesByDay(parsed);
+	}
+    };
+
+    var populate = function(viewModel) {
+	populateFromCache(viewModel);
+	populateActivities(viewModel);
+	populateProjects(viewModel);
+    };
+
     var populateProjects = function(viewModel) {
 	$.getJSON('/api/projects', function(data) {
 	    viewModel.projects(data.projects);
+	    if (localStorage !== undefined) {
+		localStorage['projects'] = JSON.stringify(data.projects);
+	    }
 	});
     };
 
     var populateActivities = function(viewModel) {
 	$.getJSON('/api/activities/2011/8', function(data) {
 	    viewModel.activitiesByDay(data.activities);
+
+	    if (localStorage !== undefined) {
+		localStorage['activitiesByDay'] = JSON.stringify(data.activities);
+	    }
+
 	});
     };
 	
-    $(document).bind("ready", function() {
+    $(document).bind("pagebeforecreate", function() {
 	var viewModel = {
 	    // Data
 	    projects: ko.observable([]),
@@ -75,7 +105,12 @@
 	    return d.toString("dddd d. MMMM yyyy").toLowerCase();
 	}, viewModel);
 
+	viewModel.postProcess = function(elements) {
+	    elements.find("input, textarea").each(function() { $(this).textinput(); });
+	    elements.find("[data-role=button]").each(function() { $(this).button(); });
+	};
+
 	ko.applyBindings(viewModel);
-	// $('.databound').each(function() { $(this).page() });
+	populate(viewModel);
     });
 }());
